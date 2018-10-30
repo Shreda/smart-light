0a3a9abee2892afde71d091558f077c19da947df
Daniel Moore <danielmoore.info@gmail.com>
2018-10-30 20:33:22 +1100

    test

--- lit.py
+++ lit.py
@@ -48,7 +48,7 @@ def colorWipe(strip, color, wait_ms=50):
     time.sleep(wait_ms/1000.0)
 
 def turnRed(strip):
-  strip.setPixelColor(1, 0xFF0000)
+  strip.setPixelColor(1, 0xFF00)
   strip.show()
 
 # Main program logic follows:
