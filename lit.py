3aa01c30b2c7a0ad5d4dc2d5e7b105bb70641ac7
Daniel Moore <danielmoore.info@gmail.com>
2018-10-30 20:46:18 +1100

    asdf

--- lit.py
+++ lit.py
@@ -75,6 +75,7 @@ if __name__ == '__main__':
       data = query_api(query=LIGHT_QUERY, variables={'id': LIGHT_ID}, url=API_URL)
       color = data['data']['light']['color']
       test_color = int(color, 16)
+      print(type(test_color))
       turnRed(strip, test_color)
       time.sleep(5)
 
